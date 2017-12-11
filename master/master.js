26548f646828cb5a7a23c66fad3803d56fd4f48b
Rory Goodman <goodmanr@tcd.ie>
2017-12-11 02:29:45 +0000

    Minimal system working locally without error handling

--- master/master.js
+++ master/master.js
@@ -8,8 +8,16 @@ var newPort = 3001
 var numWorkers=0;
 const bp = require('body-parser');
 const express = require('express');
-
+const path = require('path');
+var slavePorts = [];
 var master = express();
+var repo = "";
+var workersToConnect=process.argv[2];
+var repoFiles=[];
+var numFiles=0;
+var nextFile=0;
+
+var complexities=[];
 // parse application/x-www-form-urlencoded
 master.use(bp.json())
 master.use(bp.urlencoded({ extended: true }))
@@ -21,14 +29,68 @@ master.get('/new',function (req, res) {
   console.log("worker "+numWorkers+" connected" );
     var data = {"workerNo": numWorkers,
                 "port": newPort};
+    slavePorts[numWorkers]=newPort;
     newPort++;
     numWorkers++;
     res.send(data);
+    console.log(numWorkers);
+    console.log(workersToConnect);
+
+    if(numWorkers==workersToConnect){
+        startWork();
+    }
 })
-master.get('/work',function (req, res) {
-   console.log(JSON.parse(req.body).workerNo +" wants work"); 
-   res.send("hi");
-})
+master.post('/more',function (req, res) {
+  var worker=req.body.workerNo;
+  var fileNo=req.body.fileNo;
+  var result=req.body.result;
+  console.log("worker "+worker+" analyzed file "+fileNo+" \n score: "+result)
+  complexities[fileNo]=result;
+  if(nextFile<repoFiles.length){
+    request.post('http://localhost:'+slavePorts[worker], {form:{"file":repoFiles[nextFile],"fileNo":nextFile}});
+    nextFile++;
+  }
+  else{
+    request.post('http://localhost:'+slavePorts[worker], {form:{"file":"done"}});
+    console.log("No more files for worker "+worker);
+  }
+
+  
+
+
+});
+
+function startWork(){
+    repo = git.Clone('https://github.com/callumduffy/http-s-proxy.git', path.join(__dirname,'./repo-folder')).catch((error) =>{
+        console.log('Repo already cloned or doesnt exist');
+    }).then((repo) => {
+    //get array of js files
+    repoToArray(path.join(__dirname,'./repo-folder'), /\.js$/);
+    console.log("Cloning done...ready to start calculating");
+    console.log(repoFiles[nextFile]);
+    for(var i=0;i<numWorkers&&i<repoFiles.length;i++){
+        request.post('http://localhost:'+slavePorts[i], {form:{"file":repoFiles[nextFile],"fileNo":nextFile}});
+        nextFile++;
+    }
+})};
+
+
+function repoToArray(repoPath, fileType){
+    var files = fs.readdirSync(repoPath);
+    for (var i = 0; i < files.length; i++) {
+      var file = path.join(repoPath, files[i]);
+      var fileOrDir = fs.lstatSync(file);
+      //check if a file or directory
+      //if directory, recursion
+      if(fileOrDir.isDirectory()){
+        repoToArray(file,fileType);
+      }
+      else if(fileType.test(file)){
+        repoFiles.push(file);
+        numFiles++;
+      }
+    }
+
+};
 
 master.listen(3000);
-
--- slave/index.js
+++ slave/index.js
@@ -5,32 +5,45 @@ const util = require("util");
 const request=require('request');
 const bp = require('body-parser');
 const express = require('express');
-var repo = "https://github.com/haltu/muuri.git";
+const escomplex = require ('escomplex');
 var slave = express();
-// parse application/x-www-form-urlencoded
+var path="";
 slave.use(bp.urlencoded({ extended: false }))
 
-// parse application/json
+
 slave.use(bp.json())
 var workerNo=-1;
-function work(req,res){
-	console.log(req.body);
-}
-slave.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-  console.log("hi");
+slave.post('/',function (req, res) { 
+    console.log('Examining file: ' + req.body.fileNo );
+    if(req.body.file!="done"){
+      if(!fs.existsSync(req.body.file)){
+      console.log("file doesn't exist");
+      request.post('http://localhost:3000/more', {form:{"workerNo":workerNo,"fileNo":req.body.fileNo,"result": -1}});
+      }
+      else{
+        var fileString=fs.readFileSync(req.body.file, 'utf8');
+        var report=escomplex.analyse(fileString);
+        request.post('http://localhost:3000/more', {form:{"workerNo":workerNo,"fileNo":req.body.fileNo,"result": report.aggregate.cyclomatic}});
+      }
+    }
+    else{
+      console.log("No more files. Good work. Here's your shiny penny.")
+    }
+    
+
 })
+
 request('http://localhost:3000/new', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   workerNo=JSON.parse(body).workerNo;
 	port=JSON.parse(body).port; 
-	slave.listen(port);// Print the HTML for the Google homepage.
+  console.log(port);
+	slave.listen(port);
 });
-request.post({url:'http://localhost:3000//work', form:  {'workerNo':workerNo}}, function(err,httpResponse,body){
-	console.log("yo");
-});
+
+
+
+
 
 
